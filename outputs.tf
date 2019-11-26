da12f346875476f2dcb3dfbe7fc0652815ee9b0a
Lorenzo Aiello <lorenzo@aiello.family>
2019-11-25 17:05:24 -0800

    adding initial outputs file

---
+++ outputs.tf
@@ -0,0 +1,21 @@
+output "rds_hostname" {
+	value = aws_rds_cluster.kong.endpoint
+}
+
+output "rds_password" {
+	sensitive = true
+	value     = random_string.master_password.result
+}
+
+output "admin_token" {
+	sensitive = true
+	value = random_string.admin_token.result
+}
+
+output "lb_endpoint_external" {
+	value = aws_alb.external.dns_name
+}
+
+output "lb_endpoint_internal" {
+	value = aws_alb.internal.dns_name
+}
