96e0945bb8f86fcd19d6aca3d249065cd146f95b
Dennis Kelly <dennis.kelly@konghq.com>
2020-01-22 15:12:44 -0800

    portal_host, not admin_host

--- locals.tf
+++ locals.tf
@@ -2,5 +2,5 @@ locals {
   enable_rds = var.enable_aurora ? false : true
 
   manager_host = var.manager_host == "default" ? join("", aws_lb.internal.*.dns_name) : var.manager_host
-  portal_host  = var.portal_host == "default" ? join("", aws_lb.internal.*.dns_name) : var.admin_host
+  portal_host  = var.portal_host == "default" ? join("", aws_lb.internal.*.dns_name) : var.portal_host
 }
