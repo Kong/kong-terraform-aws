d1e087350f88072e2d1c4ca1da181cb71319b812
Dennis Kelly <dennis.kelly@konghq.com>
2020-01-22 15:12:02 -0800

    Fix LB references.

--- locals.tf
+++ locals.tf
@@ -1,6 +1,6 @@
 locals {
   enable_rds = var.enable_aurora ? false : true
 
-  manager_host = var.manager_host == "default" ? aws_lb.internal.dns_name : var.manager_host
-  portal_host  = var.portal_host == "default" ? aws_lb.internal.dns_name : var.admin_host
+  manager_host = var.manager_host == "default" ? join("", aws_lb.internal.*.dns_name) : var.manager_host
+  portal_host  = var.portal_host == "default" ? join("", aws_lb.internal.*.dns_name) : var.admin_host
 }
