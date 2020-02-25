bcec982625b2ec101426e942f6f8d09b87e0b208
Lakshmi Ravipati <LRavipati@mycwt.com>
2020-02-25 13:53:05 -0600

    [fmt-terraform] terraform fmt and github actions (#24)

--- outputs.tf
+++ outputs.tf
@@ -1,32 +1,32 @@
 output "rds_endpoint" {
-	value       = coalesce(aws_rds_cluster.kong.*.endpoint)
-	description = "The endpoint for the Kong database"
+  value       = coalesce(aws_rds_cluster.kong.*.endpoint)
+  description = "The endpoint for the Kong database"
 }
 
 output "rds_password" {
-	sensitive   = true
-	value       = random_string.db_password.result
-	description = "The database password for Kong"
+  sensitive   = true
+  value       = random_string.db_password.result
+  description = "The database password for Kong"
 }
 
 output "master_password" {
-	sensitive   = true
-	value       = random_string.master_password.result
-	description = "The master password for Kong"
+  sensitive   = true
+  value       = random_string.master_password.result
+  description = "The master password for Kong"
 }
 
 output "admin_token" {
-	sensitive   = true
-	value       = random_string.admin_token.result
-	description = "The admin token for Kong"
+  sensitive   = true
+  value       = random_string.admin_token.result
+  description = "The admin token for Kong"
 }
 
 output "lb_endpoint_external" {
-	value       = coalesce(aws_lb.external.*.dns_name)
-	description = "The external load balancer endpoint"
+  value       = coalesce(aws_lb.external.*.dns_name)
+  description = "The external load balancer endpoint"
 }
 
 output "lb_endpoint_internal" {
-	value       = coalesce(aws_lb.internal.*.dns_name)
-	description = "The internal load balancer endpoint"
+  value       = coalesce(aws_lb.internal.*.dns_name)
+  description = "The internal load balancer endpoint"
 }
