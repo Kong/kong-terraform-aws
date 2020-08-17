dc681300791c9112a399f114902653c3cc1d3b01
Matthew Tovbin <tovbinm@gmail.com>
2020-08-17 12:44:12 -0700

    Increase password length to 64 characters

--- passwords.tf
+++ passwords.tf
@@ -1,19 +1,19 @@
 resource "random_string" "master_password" {
-  length  = 32
+  length  = 64
   special = false
 }
 
 resource "random_string" "db_password" {
-  length  = 32
+  length  = 64
   special = false
 }
 
 resource "random_string" "admin_token" {
-  length  = 32
+  length  = 64
   special = false
 }
 
 resource "random_string" "session_secret" {
-  length  = 32
+  length  = 64
   special = false
 }
