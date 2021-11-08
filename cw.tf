ece48cd0cad4d80bb719aa254da28eb746ffafc4
Andres Osorio <cosoriog@gmail.com>
2021-11-08 10:09:17 -0500

    Fix reference to var.enable_external_lb in internal load balancer

--- cw.tf
+++ cw.tf
@@ -13,7 +13,7 @@ module "kong_external_lb_cw" {
 module "kong_internal_lb_cw" {
   source = "./cw/lb"
 
-  enable        = var.enable_external_lb
+  enable        = var.enable_internal_lb
   load_balancer = coalesce(join("", aws_lb.internal.*.arn_suffix), "none")
   target_group  = coalesce(join("", aws_lb_target_group.internal.*.arn), "none")
 
