a484cb783c19ee3dceb2b024a00012bac6262876
Qirui(Keery) Nie <windmgc@gmail.com>
2022-08-29 15:45:47 +0800

    chore(*) cleanup deprecation resource type (#41)
    * chore(*) add gitignore
    
    * chore(*) cleanup deprecation resource type aws_subnet_ids
    
    Co-authored-by: Cleber Rocha <105305188+cleber-rocha@users.noreply.github.com>

--- data.tf
+++ data.tf
@@ -9,7 +9,7 @@ data "aws_vpc" "vpc" {
 
 data "aws_region" "current" {}
 
-data "aws_subnet_ids" "public" {
+data "aws_subnets" "public" {
   vpc_id = data.aws_vpc.vpc.id
 
   filter {
@@ -18,7 +18,7 @@ data "aws_subnet_ids" "public" {
   }
 }
 
-data "aws_subnet_ids" "private" {
+data "aws_subnets" "private" {
   vpc_id = data.aws_vpc.vpc.id
 
   filter {
