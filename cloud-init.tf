76348950946932ecc9b7fb5545f4c75e023177cb
windmgc <windmgc@gmail.com>
2022-08-30 23:30:19 +0800

    lint

--- cloud-init.tf
+++ cloud-init.tf
@@ -1,16 +1,16 @@
 locals {
-    render_variables = {
-        DB_USER        = replace(format("%s_%s", var.service, var.environment), "-", "_")
-        CE_PKG         = var.ce_pkg
-        EE_PKG         = var.ee_pkg
-        PARAMETER_PATH = format("/%s/%s", var.service, var.environment)
-        REGION         = data.aws_region.current.name
-        VPC_CIDR_BLOCK = data.aws_vpc.vpc.cidr_block
-        DECK_VERSION   = var.deck_version
-        MANAGER_HOST   = local.manager_host
-        PORTAL_HOST    = local.portal_host
-        SESSION_SECRET = random_string.session_secret.result
-    }
+  render_variables = {
+    DB_USER        = replace(format("%s_%s", var.service, var.environment), "-", "_")
+    CE_PKG         = var.ce_pkg
+    EE_PKG         = var.ee_pkg
+    PARAMETER_PATH = format("/%s/%s", var.service, var.environment)
+    REGION         = data.aws_region.current.name
+    VPC_CIDR_BLOCK = data.aws_vpc.vpc.cidr_block
+    DECK_VERSION   = var.deck_version
+    MANAGER_HOST   = local.manager_host
+    PORTAL_HOST    = local.portal_host
+    SESSION_SECRET = random_string.session_secret.result
+  }
 }
 
 data "cloudinit_config" "cloud-init" {
